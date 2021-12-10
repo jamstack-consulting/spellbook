554b3b8684d1cce7bcda945227be92821b2ba465
Levi <levi@lvrbrtsn.com>
2021-12-10 00:29:16 -0700

    add toc and wrap plugins

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,12 +1,18 @@
 import matter from "gray-matter";
+
 import path from "path";
 import fs from "fs";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
+import rehypeToc from "rehype-toc";
+
+import rehypeWrap from "rehype-wrap";
 
 import components from "../components/mdx-components";
 import Layout from "../components/Layout";
+
 import { pageFilePaths, PAGES_PATH } from "../utils/mdxUtils";
+import React from "react";
 
 // TODO need to pick layout based on frontmatter.
 export default function Page({ source, frontMatter }) {
@@ -27,7 +33,23 @@ export async function getStaticProps({ params: { slug } }) {
     console.warn("No MDX file found for slug");
   }
 
-  const mdxSource = await serialize(content);
+  const mdxSource = await serialize(content, {
+    mdxOptions: {
+      rehypePlugins: [
+        [rehypeWrap, { wrapper: "div.page-content" }],
+        [
+          rehypeToc,
+          {
+            cssClasses: {
+              toc: "page-outline", // Change the CSS class for the TOC
+              link: "page-link", // Change the CSS class for links in the TOC
+            },
+          },
+        ],
+        // () => (tree) => console.log(JSON.stringify(tree, undefined, 2)),
+      ],
+    },
+  });
 
   return {
     props: {
